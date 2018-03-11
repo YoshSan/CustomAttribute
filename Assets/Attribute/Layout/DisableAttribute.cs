63dcda29c69612c8738c5560dd63e824af6dda6b
Yosh <yamadooo0310@gmail.com>
2018-03-11 20:10:02 +0900

    Inspector編集不可Attribute追加
    Inspector編集不可Attribute追加

---
+++ Assets/Attribute/Layout/DisableAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+
+namespace Yosh
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+	public class DisableAttribute : PropertyAttribute
+	{
+
+		public DisableAttribute() { }
+	}
+}
---
+++ Assets/Editor/CustomPropertyDrawer/DisableAttributeDrawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Yosh.Editor
+{
+	[CustomPropertyDrawer(typeof(DisableAttribute))]
+	public sealed class DisableAttributeDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var inst = attribute as DisableAttribute;
+
+			using (new EditorGUI.DisabledGroupScope(true))
+				EditorGUI.PropertyField(position, property);
+		}	
+	}
+}
