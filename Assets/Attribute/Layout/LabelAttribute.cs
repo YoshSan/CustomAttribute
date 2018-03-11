d2a220ae03d16af441d1257b136e4b6c252716a7
Yosh <yamadooo0310@gmail.com>
2018-03-11 20:42:27 +0900

    見出し表示用Attribute追加
    見出し表示用Attribute追加

---
+++ Assets/Attribute/Layout/LabelAttribute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+
+namespace Yosh
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+	public class LabelAttribute : PropertyAttribute
+	{
+		public string Label { get; private set; }
+
+		public bool IsDropShadow { get; private set; }
+
+		public LabelAttribute(string label, bool isDropShadow = false)
+		{
+			Label = label;
+			IsDropShadow = isDropShadow;
+		}
+	}
+}
---
+++ Assets/Editor/CustomPropertyDrawer/LabelAttributeDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Yosh.Editor
+{
+	[CustomPropertyDrawer(typeof(LabelAttribute))]
+	public sealed class LabelAttributeDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var inst = attribute as LabelAttribute;
+
+			if(inst.IsDropShadow)
+				EditorGUI.DropShadowLabel(position, inst.Label);
+			else
+				EditorGUI.LabelField(position, inst.Label);
+		}	
+	}
+}
