a6f149460c25af6ea4c26d49bd39b20674c918b2
Yosh <yamadooo0310@gmail.com>
2018-03-11 20:01:05 +0900

    セパレータ表示Attribute追加
    セパレータ表示Attribute追加

---
+++ Assets/Attribute/Layout/SeparatorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+
+namespace Yosh
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+	public class SeparatorAttribute : PropertyAttribute
+	{
+		public float Height { get; private set; }
+
+		public SeparatorAttribute(float height = 1)
+		{
+			Height = height;
+		}
+	}
+}
---
+++ Assets/Editor/CustomPropertyDrawer/SeparatorAttributeDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Yosh.Editor
+{
+	[CustomPropertyDrawer(typeof(SeparatorAttribute))]
+	public sealed class SeparatorAttributeDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var inst = attribute as SeparatorAttribute;
+			var rect = position;
+			rect.y += 5;
+			rect.height = inst.Height;
+			rect.width -= 10;
+			EditorGUI.LabelField(rect, "", GUI.skin.box);
+		}
+	}
+}
